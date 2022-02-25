2a776dc89fa5bba02482cf47f83fb55232e570c5
Geoff Whitehead <geoff1012@gmail.com>
2022-02-25 20:50:46 +0000

    remove unrequired generated files and refactor

---
+++ app.ts
@@ -0,0 +1,9 @@
+import {connectDB} from './services/db'
+import { start } from './app/index'
+
+(async function() {
+
+    await connectDB()
+    start()
+
+}())  
---
+++ services/db.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+export const connectDB = async () => {
+
+    try {
+        await mongoose.connect('mongodb://mongodb:27017/dev' )
+        console.log('Mongoose connected')
+
+    } catch(err) {
+        console.log('Failed to connect to db', err)
+    }
+    
+}
