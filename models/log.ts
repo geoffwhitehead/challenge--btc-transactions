8bf5727811c9e205ab4cfc601bf7d8aa43262fe7
Geoff Whitehead <geoff1012@gmail.com>
2022-02-25 20:18:29 +0000

    create transaction schema and interface

---
+++ models/log.ts
@@ -0,0 +1,50 @@
+import { Schema, model } from 'mongoose'
+
+interface Transaction {
+    involvesWatchonly: boolean,
+    account: string,
+    address: string,
+    category: TransactionCategory,
+    amount: number,
+    label: string,
+    confirmations: number,
+    blockhash: string,
+    blockindex: number,
+    blocktime: number,
+    txid: string,
+    vout: number,
+    walletconflicts: string[],
+    time: number,
+    timereceived: number,
+    'bip125-replaceable': string
+}
+
+const transactionCategory = {
+    receive: 'receive',
+    send: 'send'
+} as const
+
+type TransactionCategory = keyof typeof transactionCategory
+
+const schema = new Schema<Transaction>({
+    involvesWatchonly: {type: Boolean, required: true},
+    account: {type: String, required: true},
+    address: {type: String, required: true},
+    category: {type: String, required: true},
+    amount: {type: Number, required: true},
+    label: {type: String, required: true},
+    confirmations: {type: Number, required: true},
+    blockhash: {type: String, required: true},
+    blockindex: {type: Number, required: true},
+    blocktime: {type: Number, required: true},
+    txid: {type: String, required: true},
+    vout: {type: Number, required: true},
+    walletconflicts: {type: [String], required: true},
+    time: {type: Number, required: true},
+    timereceived: {type: Number, required: true},
+    'bip125-replaceable': {type: String, required: true},
+
+})
+
+export const TransactionModel = model<Transaction>('Transaction', schema)
+
